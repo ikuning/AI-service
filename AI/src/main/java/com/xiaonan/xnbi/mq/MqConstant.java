7bdeae9bd37f7d0fd1c2671cec560775680e011b
ikuning <122732358+ikuning@users.noreply.github.com>
2024-08-03 20:59:22 +0800

    Delete AI directory

--- AI/src/main/java/com/xiaonan/xnbi/mq/MqConstant.java
+++
@@ -1,8 +0,0 @@
-package com.xiaonan.xnbi.mq;
-
-
-public class MqConstant {
-    public static final String QUEUE_NAME = "bi_queue";
-    public static final String EXCHANGE_NAME = "bi_project_exchange";
-    public static final String ROUTING_KEY = "my_routingKey";
-}
